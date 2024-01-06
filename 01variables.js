68bc73b873bee98c349cd6b2e26001155cc94490
Siddhi-22 <maharanasiddhi@gmail.com>
2024-01-06 21:50:16 +0530

    newly started

---
+++ 01variables.js
@@ -0,0 +1,24 @@
+var score = 1000;
+var score = 101; //Redeclaring is allowed in var so not used 
+console.log(score);
+
+
+let counter = 10; // redeclaration is not possible in let but value can be changed 
+console.log(counter);
+
+
+const pi = 3.14; // no changes or reassignment is possible.
+                // also initialization must be done while declaration.
+console.log(pi);
+
+/*
+// Total 7 datatypes:
+Number
+String
+Boolean
+Null
+Undefined - declared but not initialized
+
+
+
+*/
--- first.js
+++ first.js
@@ -1,2 +1,9 @@
-var x= 10;
-alert(x);
+
+console.log("Hello World");
+
+//var x = windows.alert("siddhi");
+//console.log(x);
+
+console.log(75 - 25);
+console.log(3 + 3 - 5);
+
